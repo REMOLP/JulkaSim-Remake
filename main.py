c4592ac0b7575c671d1e9382dd39cef2f95b4efb
REMOL <remikp207@gmail.com>
2021-02-07 13:29:07 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+julkaAnsw = ["hej", "GÓWNO INCELU", "ZAMKNIJ SIĘ INCELU", "MĘŻCZYŹNI NIE POWINNI MIEĆ PRAW", "O my God... Musze posłuchać KPOPu, wkurwiles mnie Incelu", "ZAMKNIJ SIĘ INCELU", "Jajcooo", "KAPITALIZM POWODUJE NIERÓWNOŚCIIII SOCJALIZM NAJLEPSZY!!!", "MĘŻCZYŹNI NIE POWINNI MIEĆ PRAW"]
+
+
+for i in range(len(julkaAnsw)):
+    userInput = input("TY: ")
+
+    print("JULKA:", julkaAnsw[i])
+
+print("Julka rozłączyła się")
