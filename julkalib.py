96114a32ab470704a44231ab03aad31403403c8b
REMOL <remikp207@gmail.com>
2021-03-30 10:48:33 +0200

    Refaktoryzacja julkalib.py
    Szybka refaktoryzacja julkalib.py. Przeniosłem wszystkie brane dane z pliku config.json do jednej funkcji.

--- julkalib.py
+++ julkalib.py
@@ -14,106 +14,35 @@ userCustomJulkaAnsw = None
 def initConfig():
     global configFilePath
 
-    fetchCustomResp(configFilePath)
-    fetchLoopedVar(configFilePath)
-    fetchRandomizeRespVar(configFilePath)
-    fetchStoppingKeywords(configFilePath)
+    fetchConfigData(configFilePath)
 
-# Jak sama nazwa wskazuje w tej funkcji
-# brane są customowe odpowiedzi Julki
-# napisane przez użytkowników.
-def fetchCustomResp(configFilePath):
-    global userCustomJulkaAnsw
+
+def fetchConfigData(configFilePath):
+    global userCustomJulkaAnsw, loopedAnsw, randomizeAnsw, stoppingKeywords
 
     try:
         config = open(configFilePath, 'r')
     except:
         print("Coś poszło nie tak. Ścieżka do pliku konfiguracyjnego jest nieprawidłowa lub takowy plik nie istnieje.")
         return False
-    
+
     configData = json.load(config)
-    
+
     if configData["customRespStatus"] == True:
         print("'customRespStatus' w pliku konfiguracyjnym jest równy: True!")
         userCustomJulkaAnsw = configData["customResp"]
 
-    print(f"DEBUG INFO: userCustomJulkaAnsw == {userCustomJulkaAnsw}")
-    
-
-    config.close()
-
-    # Kiedy wszystko pójdzie OK to funckja zwróci 'True'.
-    # Dzięki temu zapobiegnie to trywialnym błędom i
-    # będe mógł sprawdzić czy rzeczywiście wszystko poszło
-    # dobrze z planem.
-    return True
-
-
-def fetchLoopedVar(configFilePath):
-    global loopedAnsw
-
-    try:
-        config = open(configFilePath, 'r')
-    except:
-        print("Coś poszło nie tak. Ścieżka do pliku konfiguracyjnego jest nieprawidłowa lub takowy plik nie istnieje.")
-        return False
-    
-    configData = json.load(config)
-
     loopedAnsw = configData["looped"]
-
-    print(f"DEBUG INFO: loopedAnsw == {loopedAnsw}")
-    
-
-    config.close()
-
-    # Kiedy wszystko pójdzie OK to funckja zwróci 'True'.
-    # Dzięki temu zapobiegnie to trywialnym błędom i
-    # będe mógł sprawdzić czy rzeczywiście wszystko poszło
-    # dobrze z planem.
-    return True
-
-def fetchRandomizeRespVar(configFilePath):
-    global randomizeAnsw
-
-    try:
-        config = open(configFilePath, 'r')
-    except:
-        print("Coś poszło nie tak. Ścieżka do pliku konfiguracyjnego jest nieprawidłowa lub takowy plik nie istnieje.")
-        return False
-    
-    configData = json.load(config)
-
     randomizeAnsw = configData["randomizeResp"]
-
-    print(f"DEBUG INFO: randomizeAnsw == {randomizeAnsw}")
-    
-
-    config.close()
-
-    # Kiedy wszystko pójdzie OK to funckja zwróci 'True'.
-    # Dzięki temu zapobiegnie to trywialnym błędom i
-    # będe mógł sprawdzić czy rzeczywiście wszystko poszło
-    # dobrze z planem.
-    return True
-
-
-def fetchStoppingKeywords(configFilePath):
-    global stoppingKeywords
-
-    try:
-        config = open(configFilePath, 'r')
-    except:
-        print("Coś poszło nie tak. Ścieżka do pliku konfiguracyjnego jest nieprawidłowa lub takowy plik nie istnieje.")
-        return False
-    
-    configData = json.load(config)
-
     stoppingKeywords = configData["stoppingKeywords"]
-
+    
+    
+    
+    print(f"DEBUG INFO: userCustomJulkaAnsw == {userCustomJulkaAnsw}")
+    print(f"DEBUG INFO: loopedAnsw == {loopedAnsw}")
+    print(f"DEBUG INFO: randomizeAnsw == {randomizeAnsw}")
     print(f"DEBUG INFO: stoppingKeywords == {stoppingKeywords}")
     
-
     config.close()
 
     # Kiedy wszystko pójdzie OK to funckja zwróci 'True'.
