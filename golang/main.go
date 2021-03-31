3b6c64abc686e9356edc2daa5b249160464ed945
REMOL <remikp207@gmail.com>
2021-03-31 13:48:50 +0200

    Update main.go

--- golang/main.go
+++ golang/main.go
@@ -1,18 +1,7 @@
 package main
 
-import "fmt"
+//var julkaAnsw = [...]string{"hej", "GÓWNO INCELU", "ZAMKNIJ SIĘ INCELU", "MĘŻCZYŹNI NIE POWINNI MIEĆ PRAW", "O my God... Musze posłuchać KPOPu, wkurwiles mnie Incelu", "ZAMKNIJ SIĘ INCELU", "Jajcooo", "KAPITALIZM POWODUJE NIERÓWNOŚCIIII SOCJALIZM NAJLEPSZY!!!", "MĘŻCZYŹNI NIE POWINNI MIEĆ PRAW"};
 
-var julkaAnsw = [...]string{"hej", "GÓWNO INCELU", "ZAMKNIJ SIĘ INCELU", "MĘŻCZYŹNI NIE POWINNI MIEĆ PRAW", "O my God... Musze posłuchać KPOPu, wkurwiles mnie Incelu", "ZAMKNIJ SIĘ INCELU", "Jajcooo", "KAPITALIZM POWODUJE NIERÓWNOŚCIIII SOCJALIZM NAJLEPSZY!!!", "MĘŻCZYŹNI NIE POWINNI MIEĆ PRAW"};
-
-func main(){
-	var userInput string;
-
-	for i := 0; i < len(julkaAnsw); {
-		fmt.Print("TY: ")
-		fmt.Scanln(&userInput);
-		fmt.Println("JULKA: " + julkaAnsw[i]);
-		i++;
-	}
-
-	fmt.Println("Julka zablokowała Cię...")
+func main() {
+	fetchConfigData();
 }
